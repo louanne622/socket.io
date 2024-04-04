1a4b237d553cb3636f222f32b0e297537e8a3d9e
Lou-Anne LECOCQ <lecocqpro1@gmail.com>
2024-04-04 13:54:58 +0200

    feat(index.js): send message between server and client

--- index.js
+++ index.js
@@ -14,16 +14,14 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
 
   io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);   
     });
   });
+
+
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
