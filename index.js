882606b4ffe21f633f630b12c90e00b3fcf346df
Lou-Anne LECOCQ <lecocqpro1@gmail.com>
2024-04-04 13:43:26 +0200

    feat(index.html): add html page

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
 const app = express();
 const server = createServer(app);
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Salut la team</h1>');
 });
 
 server.listen(3000, () => {
